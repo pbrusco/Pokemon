09db2a1688edc396b8e9b23c1e83479c3209c218
Pablo Brusco <pablo.brusco@gmail.com>
2026-04-06 14:03:26 -0300

    new maps structure

--- src/data/maps/index.ts
+++ src/data/maps/index.ts
@@ -1,23 +1,23 @@
-import pallet_town from './pallet_town.json';
-import oaks_lab from './oaks_lab.json';
-import route_1 from './route_1.json';
+import { parseTileMap } from './tileParser';
+
+import pallet_town   from './pallet_town.json';
+import oaks_lab      from './oaks_lab.json';
+import route_1       from './route_1.json';
 import viridian_city from './viridian_city.json';
-import pokecenter from './pokecenter.json';
-import pokemart from './pokemart.json';
+import pokecenter    from './pokecenter.json';
+import pokemart      from './pokemart.json';
 import viridian_forest from './viridian_forest.json';
-import pewter_city from './pewter_city.json';
-import pewter_gym from './pewter_gym.json';
-import route_3 from './route_3.json';
+import pewter_city   from './pewter_city.json';
+import pewter_gym    from './pewter_gym.json';
+import route_3       from './route_3.json';
 
-import { Tile } from '../../types';
-
-export const MAP_PALLET_TOWN = pallet_town as Tile[][];
-export const MAP_OAKS_LAB = oaks_lab as Tile[][];
-export const MAP_ROUTE_1 = route_1 as Tile[][];
-export const MAP_VIRIDIAN_CITY = viridian_city as Tile[][];
-export const MAP_POKECENTER = pokecenter as Tile[][];
-export const MAP_POKEMART = pokemart as Tile[][];
-export const MAP_VIRIDIAN_FOREST = viridian_forest as Tile[][];
-export const MAP_PEWTER_CITY = pewter_city as Tile[][];
-export const MAP_PEWTER_GYM = pewter_gym as Tile[][];
-export const MAP_ROUTE_3 = route_3 as Tile[][];
+export const MAP_PALLET_TOWN    = parseTileMap(pallet_town);
+export const MAP_OAKS_LAB       = parseTileMap(oaks_lab);
+export const MAP_ROUTE_1        = parseTileMap(route_1);
+export const MAP_VIRIDIAN_CITY  = parseTileMap(viridian_city);
+export const MAP_POKECENTER     = parseTileMap(pokecenter);
+export const MAP_POKEMART       = parseTileMap(pokemart);
+export const MAP_VIRIDIAN_FOREST = parseTileMap(viridian_forest);
+export const MAP_PEWTER_CITY    = parseTileMap(pewter_city);
+export const MAP_PEWTER_GYM     = parseTileMap(pewter_gym);
+export const MAP_ROUTE_3        = parseTileMap(route_3);
